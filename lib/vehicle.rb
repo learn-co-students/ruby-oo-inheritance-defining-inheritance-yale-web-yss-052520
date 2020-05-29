000bf6a37ab3a797a2ed628e83013084ec606aae
Eric Ou <eric.ou@yale.edu>
2020-05-29 13:42:21 -0700

    Done.

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,17 @@
 class Vehicle
 
+    attr_accessor :wheel_number, :wheel_size
+    def initialize(wheel_size, wheel_num)
+        @wheel_size = wheel_size
+        @wheel_number = wheel_num
+    end
+
+    def go()
+        "vrrrrrrrooom!"
+    end
+
+    def fill_up_tank()
+        "filling up!"
+    end
+
 end
