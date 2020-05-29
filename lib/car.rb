1b4c6e8e3fae5600a3653943aaabfe846d5c9be2
Drew Beckmen <drew.beckmen@yale.edu>
2020-05-29 14:20:41 -0400

    Done.

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,8 @@
 require_relative "./vehicle.rb"
+
+# We define Car as a subclass of Vehicle in the following way:
+class Car < Vehicle 
+    def go
+        "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+    end 
+end 
